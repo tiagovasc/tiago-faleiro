4c414ffb84606c71eae1916c32399b2266ece18c
suntoes <mar.suntoes@gmail.com>
2023-08-21 22:13:05 +0800

    up

--- src/compositions/last-paragraph.tsx
+++ src/compositions/last-paragraph.tsx
@@ -8,16 +8,16 @@ export default function LastParagraph(): JSX.Element {
       className="flex flex-col items-center pb-20 gap-10"
     >
       <div className="max-w-xl lg:max-w-4xl mx-10 ">
-        <About.P className="inline mx-0">
+        <About.P className="!inline !mx-0">
           Embarking on your fitness journey with a knowledgeable and dedicated
           coach can make all the difference.
         </About.P>{' '}
-        <About.P className="inline mx-0">
+        <About.P className="!inline !mx-0">
           Together, we can create a personalized plan that aligns with your
           lifestyle and goals, ensuring that you make sustainable, long-term
           progress.
         </About.P>{' '}
-        <About.P className="inline mx-0">
+        <About.P className="!inline !mx-0">
           If you're ready to take the next step, I'm here to guide and support
           you every step of the way.
         </About.P>{' '}
